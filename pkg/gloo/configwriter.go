7348d265ea5db9551e0b6e08e4824d0212777803
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-06-20 14:20:43 -0400

    added a config writer TODO implementation

---
+++ pkg/gloo/configwriter.go
@@ -0,0 +1,15 @@
+package gloo
+
+import (
+	"github.com/hashicorp/consul/api"
+	"github.com/solo-io/cgb/pkg/consul"
+)
+
+type GlooConfigWriter struct {
+}
+
+func (g *GlooConfigWriter) Write(cfg *api.ProxyInfo) error {
+	panic("TODO")
+}
+
+var _ consul.ConfigWriter = &GlooConfigWriter{}
