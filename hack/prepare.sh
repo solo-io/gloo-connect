5b40a0055818802dfe5a4e2ae02240483d35856e
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-06-26 05:29:35 -0400

    using gloo deps

--- hack/prepare.sh
+++ hack/prepare.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
 ln -s $PWD/pkg/consul/hack/agentconnect.go vendor/github.com/hashicorp/consul/api/
-cp -r ../gloo/pkg/plugins/connect pkg/gloo/
-cp -r ../gloo/pkg/* vendor/github.com/solo-io/gloo/pkg/
