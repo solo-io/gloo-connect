35fde0dae79450d4187c4c46d5fbdec47969f641
Mitch Kelley <mitch.kelley@solo.io>
2018-07-08 10:17:53 -0400

    updates from code review

--- cmd/set/set.go
+++ cmd/set/set.go
@@ -27,7 +27,7 @@ func Cmd(rc *runner.RunConfig) *cobra.Command {
 
 func cmdSetServices(rc *runner.RunConfig) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "service",
+		Use:   "service [service_name]",
 		Short: "set service",
 		RunE: func(c *cobra.Command, args []string) error {
 			err := validateServiceParams(serviceFlags, args)
@@ -44,7 +44,7 @@ func cmdSetServices(rc *runner.RunConfig) *cobra.Command {
 			return gc.ConfigureService(args[0], serviceFlags.retries)
 		},
 	}
-	cmd.PersistentFlags().Uint32VarP(&serviceFlags.retries, "retries", "", 0, "number of retries")
+	cmd.PersistentFlags().Uint32VarP(&serviceFlags.retries, "retries", "", 0, "max number of http connection retries. Value of \"0\" specifies continuous connection retries. Default 0")
 	cmd.PersistentFlags().BoolVarP(&serviceFlags.http, "http", "", false, "whether http mode should be used, default false")
 	return cmd
 }
@@ -60,9 +60,9 @@ func validateServiceParams(flags serviceFlagsType, args []string) error {
 	if len(args) != 1 {
 		return errors.New("must pass a single argument")
 	}
-	// if !flags.http {
-	// 	// TODO(mitchdraft) Do we want this constraint? Or flag?
-	// 	return errors.New("must explicitly specify the --http flag when using `set service`")
-	// }
+	if !flags.http {
+		// This constraint exists to ensure that the user understands that the service will be provided over http
+		return errors.New("must explicitly specify the --http flag when using `set service`")
+	}
 	return nil
 }
