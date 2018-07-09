23be0df89e98f6268c6d78d25501b6d6a4e96ad7
Mitch Kelley <mitch.kelley@solo.io>
2018-07-08 20:36:50 -0400

    break bridge and http out of main

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/solo-io/gloo-connect/pkg/cmd"
+	"github.com/solo-io/gloo-connect/pkg/cmd/bridge"
 	"github.com/solo-io/gloo-connect/pkg/cmd/get"
+	"github.com/solo-io/gloo-connect/pkg/cmd/http"
 	"github.com/solo-io/gloo-connect/pkg/cmd/set"
 	"github.com/solo-io/gloo-connect/pkg/runner"
 	"github.com/solo-io/gloo/pkg/bootstrap"
-	"github.com/solo-io/gloo/pkg/bootstrap/configstorage"
 	"github.com/solo-io/gloo/pkg/bootstrap/flags"
 	"github.com/spf13/cobra"
 )
@@ -28,17 +28,6 @@ var rootCmd = &cobra.Command{
 	Short: "root command for running and managing gloo-connect",
 }
 
-func run() error {
-	store, err := configstorage.Bootstrap(rc.Options)
-	if err != nil {
-		return err
-	}
-	if err := runner.Run(rc, store); err != nil {
-		return err
-	}
-	return nil
-}
-
 func initRunnerConfig(c *runner.RunConfig) {
 	// always use consul for storage and service discovery
 	c.Options.ConfigStorageOptions.Type = bootstrap.WatcherTypeConsul
@@ -51,35 +40,5 @@ func init() {
 
 	initRunnerConfig(&rc)
 
-	bridgeCmd.PersistentFlags().StringVar(&rc.GlooAddress, "gloo-address", "127.0.0.1", "bind address where gloo should serve xds config to envoy")
-	bridgeCmd.PersistentFlags().UintVar(&rc.GlooPort, "gloo-port", 8081, "port where gloo should serve xds config to envoy")
-	bridgeCmd.PersistentFlags().BoolVar(&rc.UseUDS, "gloo-uds", false, "use unix domain socket for gloo and envoy")
-	bridgeCmd.PersistentFlags().StringVar(&rc.ConfigDir, "conf-dir", "", "config dir to hold envoy config file")
-	bridgeCmd.PersistentFlags().StringVar(&rc.EnvoyPath, "envoy-path", "", "path to envoy binary")
-
-	rootCmd.AddCommand(bridgeCmd, httpCmd, get.Cmd(&rc), set.Cmd(&rc))
-}
-
-var bridgeCmd = &cobra.Command{
-	Use:   "bridge",
-	Short: "runs gloo-connect to bridge Envoy to Consul's connect api",
-	RunE: func(_ *cobra.Command, args []string) error {
-		return run()
-	},
-}
-
-var httpCmd = &cobra.Command{
-	Use:   "http",
-	Short: "manage HTTP features for in-mesh services",
-	Long:  "",
-	RunE: func(_ *cobra.Command, args []string) error {
-		store, err := configstorage.Bootstrap(rc.Options)
-		if err != nil {
-			return err
-		}
-
-		gc := cmd.GlooClient{Store: store}
-
-		return gc.Demo()
-	},
+	rootCmd.AddCommand(bridge.Cmd(&rc), http.Cmd(&rc), get.Cmd(&rc), set.Cmd(&rc))
 }
